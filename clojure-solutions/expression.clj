9c46bc271f6592afd4edda1d19fcfaa349474fff
Marlesss <60238634+Marlesss@users.noreply.github.com>
2022-05-04 01:37:12 +0300

    HW11 base completed how to differentiate pow and log??77?7??7?

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -1,6 +1,7 @@
 ;; hard pls
 ;; hard pls
 ;; hard pls
+(defn abs [x] (if (>= x 0) x (- x)))
 (defn proto-get
   ([obj key] (proto-get obj key nil))
   ([obj key default]
@@ -18,54 +19,53 @@
 (def _calc (field :calc))
 (def _exprs (field :exprs))
 (def _value (field :value))
+(def _neutral (field :neutral))
 (def toString (method :toString))
 (def evaluate (method :evaluate))
-(def OperationProto
-  {:toString (fn [this] (str "(" (_sign this) " " (clojure.string/join " " (map toString (_exprs this))) ")"))
-   :evaluate (fn [this] (fn [map] (apply _calc this (mapv evaluate _exprs))))
-   })
-(defn Operation [this calc & exprs]
-  (assoc this
-    :calc calc
-    :exprs exprs))
-(def _Operation (constructor Operation OperationProto))
+(def diff (method :diff))
 (defn Constant [value]
   {:value    value
-   :toString (fn [this] (str value))
-   :evaluate (constantly value)})
+   :toString (fn [this] (str (_value this)))
+   :evaluate (fn [this vars] (_value this))
+   :diff     (fn [this var] (Constant 0))})
 (defn Variable [value]
   {:value    value
    :toString _value
-   :evaluate (fn [this] (fn [map & args] (println "GOT: " map "|" args) (get map name)))})
-;(defn constant [c] (constantly c))
-;(defn variable [var] (fn [map] (get map var)))
-;(defn operation [f neutral & exprs]
-;  (let [exprs (if (== (count exprs) 1) (conj exprs (constant neutral)) exprs)]
-;    (fn [map] (reduce f (mapv #(% map) exprs)))))
-;(defn add [& exprs] (apply operation + 0 exprs))
-;(defn subtract [& exprs] (apply operation - 0 exprs))
-;(defn multiply [& exprs] (apply operation * 1 exprs))
-;(defn divide [& exprs] (apply operation (fn [^double x ^double y] (/ x y)) 1 exprs)) ;; (apply / [1.0 0.0])
-;(defn negate [expr] (subtract expr))
-;(defn mean [& exprs] (divide (apply add exprs) (constant (count exprs))))
-;(defn varn [& exprs] (subtract (apply mean (mapv #(multiply % %) exprs)) (#(multiply % %) (apply mean exprs))))
-;(defn pow [expr1 expr2] (operation #(java.lang.Math/pow %1 %2) 0 expr1 expr2))
-;(defn abs [x] (if (>= x 0) x (- x)))
-;(defn log [expr1 expr2] (operation #(/ (java.lang.Math/log (abs %2)) (java.lang.Math/log (abs %1))) 0 expr1 expr2))
-;(def funcs {"+" add "-" subtract "*" multiply "/" divide "negate" negate "mean" mean "varn" varn "pow" pow "log" log})
-;(defn parser [inp] (if (seq? inp)
-;                     (apply (get funcs (name (first inp))) (mapv parser (rest inp)))
-;                     (if (number? inp) (constant inp) (variable (name inp)))))
-;(defn parseFunction [inp] (parser (read-string inp)))
-(println "cons")
-(def const (Constant 5))
-(println const)
-(println (evaluate const {"x" 5}))
-(println (evaluate const {"x" 6}))
-(println (evaluate const {"x" 7}))
-(println "variable")
-(def variable (Variable "x"))
-(println variable)
-(println (evaluate variable {"x" 5}))
-(println (evaluate variable {"x" 6}))
-(println (evaluate variable {"x" 7}))
+   :evaluate (fn [this vars] (get vars (_value this)))
+   :diff     (fn [this var] (if (= (_value this) var) (Constant 1) (Constant 0)))})
+(def _Operation)
+(def OperationProto
+  {:toString (fn [this] (str "(" (_sign this) " " (clojure.string/join " " (map toString (_exprs this))) ")"))
+   :evaluate (fn [this vars]
+               (let [exprs (if (== (count (_exprs this)) 1) (conj (_exprs this) (Constant (_neutral this))) (_exprs this))]
+                 (reduce (_calc this) (map #(evaluate %1 vars) exprs))))
+   :diff     (fn [this var] (apply _Operation (_calc this) (_sign this) (_neutral this) (map #(diff %1 var) (_exprs this))))})
+(defn Operation [this calc sign neutral & exprs]
+  (assoc this
+    :calc calc
+    :sign sign
+    :neutral neutral
+    :exprs exprs))
+(def _Operation (constructor Operation OperationProto))
+(defn Add [& exprs] (apply _Operation + "+" 0 exprs))
+(defn Subtract [& exprs] (apply _Operation - "-" 0 exprs))
+(defn Multiply [& exprs]
+  (assoc (apply _Operation * "*" 1 exprs)
+    :diff (fn [this var]
+            (if (== (count (_exprs this)) 1) (diff (first (_exprs this)) var)
+                                             (Add (apply Multiply (diff (first (_exprs this)) var) (rest (_exprs this)))
+                                                  (Multiply (diff (apply Multiply (rest (_exprs this))) var) (first (_exprs this))))))))
+(defn Divide [& exprs]
+  (assoc (apply _Operation (fn [^double x ^double y] (/ x y)) "/" 1 exprs)
+    :diff (fn [this var] (let [exprs (if (== (count (_exprs this)) 1) (conj (_exprs this) (Constant (_neutral this))) (_exprs this))
+                               first (first exprs) second (apply Multiply (rest exprs))]
+                           (Divide (Subtract (Multiply (diff first var) second) (Multiply (diff second var) first))
+                                   (Multiply second second))))))
+(defn Negate [expr] (_Operation - "negate" 0 expr))
+(defn Pow [expr1 expr2] (_Operation #(Math/pow %1 %2) "pow" 0 expr1 expr2))
+(defn Log [expr1 expr2] (_Operation #(/ (Math/log (abs %2)) (Math/log (abs %1))) "log" 0 expr1 expr2))
+(def funcs {"+" Add "-" Subtract "*" Multiply "/" Divide "negate" Negate "pow" Pow "log" Log})
+(defn parser [inp] (if (seq? inp)
+                     (apply (get funcs (name (first inp))) (mapv parser (rest inp)))
+                     (if (number? inp) (Constant inp) (Variable (name inp)))))
+(defn parseObject [inp] (parser (read-string inp)))
